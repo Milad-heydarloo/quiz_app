73fcbfcecd46e85ba4b54dddf7c3aa655d109b3f
milad <78633834+Milad-heydarloo@users.noreply.github.com>
2023-07-31 06:25:15 -0700

    vazifash negahdariyeh mojodiyat soal hast in class

---
+++ lib/data/Question.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+//vazifash negahdariyeh mojodiyat soal hast in class
+class question {
+  String? titleQuestion;
+  String? imageQuestionNumber;
+  //list item javaba toshe ya gozinehha
+  List<String>? answerList;
+  //javab drost toshe
+  int? correctAnswer;
+}
