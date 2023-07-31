58e63131b76f41bbd1e38128acd5f67060681c4e
milad <78633834+Milad-heydarloo@users.noreply.github.com>
2023-07-31 03:07:52 -0700

    asset: fonts/dm.ttf

--- lib/main.dart
+++ lib/main.dart
@@ -3,14 +3,10 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(const _application());
 }
-class _application extends StatefulWidget {
-  const _application({super.key});
 
-  @override
-  State<_application> createState() => __applicationState();
-}
+class _application extends StatelessWidget {
+  const _application({Key? key}) : super(key: key);
 
-class __applicationState extends State<_application> {
   @override
   Widget build(BuildContext context) {
     return Container();
